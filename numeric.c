71295e2707f8f9624056a8ebbb4df058d785f38d
Revathi N <nrevathibe38@gmail.com>
2016-07-06 11:08:02 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char b[10];
+printf("Enter the string:\n");
+scanf("%s",b);
+if (%is digit(b))
+{
+printf("False\n");
+}
+else
+{
+printf("True\n");
+}
+getch();
+}
